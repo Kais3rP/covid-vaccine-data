3011c8e85acd3d835596c637c82c72b94790b305
kais3rp <cesare@polonara.com>
2023-03-25 02:52:54 +0100

    fix: fixed badges

--- src/features/graphs/BadgeTextGraph.js
+++ src/features/graphs/BadgeTextGraph.js
@@ -1,51 +1,67 @@
-import { Container, Typography, Box } from '@mui/material'
-import { Badge } from '../../components/reusable/Badge'
-import React from 'react'
-import gum from '../../img/gum.png'
+import { Container, Typography, Box } from "@mui/material";
+import { Badge } from "../../components/reusable/Badge";
+import React from "react";
+import gum from "../../img/gum.png";
+import Cockade from "../../components/reusable/svg/Cockade";
 
 const BadgeTextGraph = ({ title, data, badgePosition }) => {
   return (
     <Container
       sx={{
-        backgroundColor: 'primary.main',
+        backgroundColor: "primary.main",
         p: 6,
         pb: 1,
-        position: 'relative',
+        position: "relative",
         mt: 6,
         mb: 2,
       }}
       maxWidth="false"
     >
-      <Badge
-        alt="coccarda"
-        width={6}
-        left={badgePosition === 'left' ? '20px' : '100%'}
-        top="-40px"
-        position={'absolute'}
-      />
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          transform: "translateX(-50%) translateY(-25%)",
+          width: "4rem",
+          mr: 4,
+        }}
+      >
+        <Cockade />
+      </Box>
 
-      <Typography color={'text.primary'} variant={'h6'} align={'center'}>
+      <Typography color={"text.primary"} variant={"h6"} align={"center"}>
         {title}
       </Typography>
       <Typography
         sx={{ mt: 2 }}
-        color={'text.primary'}
-        variant={'h3'}
-        align={'center'}
+        color={"text.primary"}
+        variant={"h3"}
+        align={"center"}
       >
         {data}
       </Typography>
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
+          display: "flex",
+          justifyContent: "flex-end",
         }}
       >
-        {' '}
-        <Badge src={gum} alt="gum" width={3.5} />
+        <Box
+          sx={{
+            position: "absolute",
+            top: "20px",
+            right: 0,
+            // transform: "translateX(-50%) translateY(-25%)",
+            width: "4rem",
+            mr: 4,
+          }}
+        >
+          <img src={gum} alt="gum" />
+        </Box>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default BadgeTextGraph
+export default BadgeTextGraph;
